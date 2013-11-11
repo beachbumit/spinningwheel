8f986e5033382439626358cdb14e51c89177a200
Aaron Young <aarongyoung85@gmail.com>
2013-11-11 15:25:27 -0800

    converted to OOP javascript

--- js/functions.js
+++ js/functions.js
@@ -1,26 +1,46 @@
 function wheelGame() {
 	var stop;
-	var next;
-	var first = $('#one');
-	var circles = ['one', 'two', 'three', 'four', 
-					'five', 'six', 'eight', 'nine', 
-					'ten', 'eleven', 'twelve', 'thirteen', 
-					'fourteen', 'fifteen', 'sixteen', 'seventeen', 
-					'nineteen', 'twenty', 'twentyone', 'twentytwo'];
-	
-	this.startGame = function(li) {
-		
+	var i = 0;
+	var score = [];
+	var list = $('#circles').children();
+	this.total = 0;
+
+	this.startGame = function() {
+		stop = setInterval(function() {
+			$(list[i]).css('color', 'black').css('border', '2px solid black').attr('data-win', 'none');
+			setTimeout(function() {
+				$(list[i]).css('color', 'red').css('border', '2px solid red').css('font-weight', 'bold').attr('data-win', 'win');
+			}, 5);
+			i++;
+			if (i == list.length) {
+				i = 0;
+			}
+		}, 40);
 	}
 
-	var spinning = function(li) {
-		li.next().css('color', 'red').css('border', '1px solid red');
-	}
-
-	var clear = function(li) {
-		li.css('color', 'black').css('border', '1px solid black');
-	}
-
-	this.stopInterval = function() {
+	this.stopGame = function() {
 		clearInterval(stop);
 	}
+
+	this.getScore = function() {
+		this.total = 0;
+		score.push($('#circles').find("li[data-win=win]").attr('data-score'));
+		if(score.length > 5) {
+			var last = score.pop();
+			score = [];
+			score.push(last);
+		}
+		for(var i=0; i<score.length; i+=1) {
+			this.total += parseInt(score[i]);
+		}
+		return this.total
+	}
+	this.checkTries = function() {
+		if(score.length == 5) {
+			return this.total;
+		} else {
+			return false;
+		}
+	}
+
 }
