7562beb412323938de36bb1d4dbe6dcc9b9ba221
Aaron Young <aarongyoung85@gmail.com>
2013-11-11 13:17:34 -0800

    wheel spins once around

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,23 @@
 function wheelGame() {
 	var stop;
+	var next;
 	var first = $('#one');
 	var circles = ['one', 'two', 'three', 'four', 
 					'five', 'six', 'eight', 'nine', 
 					'ten', 'eleven', 'twelve', 'thirteen', 
 					'fourteen', 'fifteen', 'sixteen', 'seventeen', 
 					'nineteen', 'twenty', 'twentyone', 'twentytwo'];
-	this.startGame = function() {
-		stop = setInterval(spinning, 100);
+	
+	this.startGame = function(li) {
+		
 	}
 
-	var spinning = function() {
-		var random = circles[]
-		first.css('color', 'rgb(255, 0, 0)');
+	var spinning = function(li) {
+		li.next().css('color', 'red').css('border', '1px solid red');
+	}
+
+	var clear = function(li) {
+		li.css('color', 'black').css('border', '1px solid black');
 	}
 
 	this.stopInterval = function() {
